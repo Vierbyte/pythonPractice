fada439f0ae3293206c46f02b9c51175a22fce7a
Corey <corey.lindsey.r@gmail.com>
2022-02-19 15:07:09 -0500

    python 101 file.

---
+++ 101.py
@@ -0,0 +1,72 @@
+print("Hello World!!!")
+
+'''first_name = "Vier";
+1
+2
+3
+comment test
+'''
+
+
+
+print("                 ")
+
+
+# """
+# comment test
+# """
+
+print("I've returned to conquer you!" + "  " + "After I eat of course!")
+
+# #comment test print("test me")
+
+
+# #String 
+# "HELLO"
+
+# #INTERGER 
+# 25
+
+# #FLOAT 
+# 25.5
+
+# #BOOLEAN 
+# print(true)
+# print(false)
+
+# #LIST 
+# [2, 3, 4, 5, 6, "hi", false]
+
+# #VARIABLES
+students = ["Awa", "Tony", "Hilda"]
+print(students)
+
+canAccess = True
+if canAccess:
+     print("access granted!")
+     if False:
+         print("access denied.")
+        
+
+print(canAccess)
+
+#TUPLE a list that values can't be changed
+car_vins = ("23oinfo123")
+print(car_vins)
+
+#DICTIONARIES key value pairs / aka objects in javascript and java
+cor_class = {
+    "teacher": "troy",
+    "students": ["Mike", "Robbie", "Ronnie", "Hosea"]
+}
+
+user = {
+    "first_name": "Corey",
+    "last_name": "Lindsey",
+    "age": "28",
+}
+print(cor_class["students"][2])
+
+print(user["age"])
+
+print()
